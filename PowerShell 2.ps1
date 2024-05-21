f487e567884a102d6a16240c8cbf2a6c218ee83a
Misfit51 <116864644+Misfit51@users.noreply.github.com>
2024-05-21 09:27:39 -0400

    Update PowerShell 2.ps1

--- PowerShell 2.ps1
+++ PowerShell 2.ps1
@@ -1,2 +1,26 @@
 Jagged Array, foreach (<condition>), Do until, Process block, While,
 
+$BB = " "
+
+foreach($fish in $BB){
+    if ($fish -like 'SPONGE'){
+
+***This regex string will help later***  '^L.*r$' -match 'Larry the lobster'
+
+$num = 0
+$magic = 182
+while($True){
+    $num = [int](Read-Host -Prompt "Pick a number between 0 and 200")
+    if ($num -lt $magic){
+        "Too low!"
+    }
+    elseif($num -gt $magic){
+        "Too high"
+    }
+    else{
+        "blink-$magic"; break
+    }
+    
+}
+
+
